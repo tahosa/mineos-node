ca90bd29c7d8922417fbfc103ad71b87a057c196
Randall Crock <rlcrock@gmail.com>
2024-08-05 20:07:18 -0600

    refactor bedrock templates

--- src/profiles.d/template.ts
+++ src/profiles.d/template.ts
@@ -1,25 +1,16 @@
 export default class profile {
-  id: string | null;
-  time: number | null;
-  releaseTime: number | null;
-  type: 'release' | 'snapshot' | 'old_version' | null;
-  group: string | null;
-  webui_desc: string | null;
-  weight: number;
-  downloaded: boolean;
-  filename: string | null;
-  version: string | number | null;
+  id?: string;
+  time?: number;
+  releaseTime?: number;
+  type?: 'release' | 'snapshot' | 'old_version';
+  group?: string;
+  webui_desc?: string;
+  weight: number = 0;
+  downloaded: boolean = false;
+  filename?: string;
+  version?: string | number;
+  release_version?: string;
+  url?: string;
 
-  constructor() {
-    this.id = null;
-    this.time = null;
-    this.releaseTime = null;
-    this.type = null; //
-    this.group = null; //mojang, ftb, ftb_third_party, pocketmine, etc.
-    this.webui_desc = null;
-    this.weight = 0;
-    this.downloaded = false;
-    this.filename = null; // minecraft_server.1.8.8.jar
-    this.version = null; // 1.8.8,
-  }
+  constructor() {}
 }
